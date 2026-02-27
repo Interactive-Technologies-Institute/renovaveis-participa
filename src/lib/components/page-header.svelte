04a6b332d5dc11b089943594c37133e6fd11e08a
João Nogueira <jr_nogueira1@msn.com>
2026-02-27 01:04:46 +0000

    Add per page title

--- src/lib/components/page-header.svelte
+++ src/lib/components/page-header.svelte
@@ -3,6 +3,11 @@
 	export let description: string | undefined = undefined;
 </script>
 
+<svelte:head>
+	<title>{title} | Renováveis Participa</title>
+	<meta name="title" content="{title} | Renováveis Participa" />
+</svelte:head>
+
 <div class="relative w-full overflow-hidden">
 	<div class="dotted-grid-bg absolute inset-0 z-0"></div>
 	<div class="relative z-10 container mx-auto px-4 py-12 text-center md:py-20">
